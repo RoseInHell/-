c05bf4f9918a2f10e3e422bd6ac7cee9a2f5d94f
RoseInHell <242108590@qq.com>
2021-07-18 23:26:39 +0800

    js

--- 1.js
+++ 1.js
@@ -1,91 +1,27 @@
-function demo() {
-  setTimeout(demo, 0)
+function createAnother(child, parent) {
+  var prototype = Object.create(parent.prototype);
+  console.log(prototype)
+  parent.constructor = child;
+  child.prototype = prototype;
+}
+function Parent(name) {
+  this.name = name;
+  this.color = [1,2,3]
 }
 
-function demo2() {
-  return Promise.resolve().then(demo2)
-}
-// demo2()
-
-// 1. 给定一个二叉树，找出其最大深度。
-// 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
-// 说明: 叶子节点是指没有子节点的节点。
-// 示例：
-  //   3
-
-  //  / \
-
-  // 9  20
-
-  //   /  \
-
-  //  15   7
-
-// 返回它的最大深度 3 。
-```javascript
-
- function TreeNode(val) {
-
-     this.val = val;
-
-     this.left = this.right = null;
-
- }
-
-const n1 = new TreeNode(7)
-
-const n2 = new TreeNode(15)
-
-const n3 = new TreeNode(20)
-
-n3.left = n2
-
-n3.right = n1
-
-const n4 = new TreeNode(9)
-
-const n5 = new TreeNode(3)
-
-n5.left = n4
-
-n5.right = n3
-
-const tree = n5
-
-const maxDepth = function(root) {
-
+Parent.prototype.sayName = function() {
+  console.log(this.name);
 }
 
-```
-const maxDepth = function(root) {
-	if (!root) return 0;
-	let deep = Math.max(maxDepth(root.left),maxDepth(root.right))
-	return deep+1
+function Child(name, age) {
+  this.age = age;
+  Parent.call(this, name);
 }
- function TreeNode(val) {
 
-     this.val = val;
+createAnother(Child, Parent)
 
-     this.left = this.right = null;
+var person = {
+  name: '1'
+}
 
- }
-const n1 = new TreeNode(7)
-
-const n2 = new TreeNode(15)
-
-const n3 = new TreeNode(20)
-
-n3.left = n2
-
-n3.right = n1
-
-const n4 = new TreeNode(9)
-
-const n5 = new TreeNode(3)
-
-n5.left = n4
-
-n5.right = n3
-
-const tree = n5
-console.log(maxDepth(tree))
+console.log(Object.create(person))
