2e5719a25ab5fd47923a79968a56f42b9c3bae6f
RoseInHell <2421085090@qq.com>
2021-08-20 17:56:23 +0800

    11

--- network/src/components/header/index.js
+++ network/src/components/header/index.js
@@ -1,10 +1,29 @@
-import React, { memo } from 'react'
-import './index.less'
+import React, { memo, Fragment } from 'react'
+import { NavLink } from 'react-router-dom'
+import './index.scss'
+import { headerLinks } from '../../common/local-data'
 
 export default memo(function Header() {
+  
   return (
     <div className="app-header">
-      123
+      <div className="content  wrap-v1">
+        <div className="left_wrapped">
+          <a href="#/" className="logo sprite_01">网易云音乐</a>
+          <div className="left_title">
+            {
+              headerLinks.map(item => {
+                return (
+                  <NavLink key={item.title} to={item.link}>
+                    {item.title}
+                  </NavLink>
+                )
+              })
+            }
+          </div>
+        </div>
+      </div>
+
     </div>
   )
 })
